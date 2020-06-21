a1e23967aaaf4e05a58dbb4d016904d73a438025
HectorGarciaGTO <prohecgh@gmail.com>
2020-06-21 14:30:00 -0500

    Dockers

--- src/app/services/cubo-northwind.service.ts
+++ src/app/services/cubo-northwind.service.ts
@@ -9,27 +9,27 @@ export class CuboNorthwindService {
   constructor(private http: HttpClient) {}
   getTop5(dimension: string, anio: string, mes: string, order: string) {
     if (anio === '' && mes === '')
-      return this.http.get(`https://localhost:44342/backend/Top5/${dimension}`);
+      return this.http.get(`http://192.168.1.76/API.Northwind.ISSC811/backend/Top5/${dimension}`);
     return this.http.get(
-      `https://localhost:44342/backend/Top5/${dimension}/${anio}/${mes}`
+      `http://192.168.1.76/API.Northwind.ISSC811/backend/Top5/${dimension}/${anio}/${mes}`
     );
   }
 
   getHistorico(dimension: string, elemento: string) {
     return this.http.get(
-      `https://localhost:44342/backend/HistoricoMesAnual/${dimension}/${elemento}`
+      `http://192.168.1.76/API.Northwind.ISSC811/backend/HistoricoMesAnual/${dimension}/${elemento}`
     );
   }
 
   getMembers(dimension: string) {
     return this.http
-      .get(`https://localhost:44342/backend/GetMembers/${dimension}`)
+      .get(`http://192.168.1.76/API.Northwind.ISSC811/backend/GetMembers/${dimension}`)
       .pipe(map((result: any) => result.datosDimension));
   }
 
   getDimTiempo(granularidad: string) {
     return this.http.get(
-      `https://localhost:44342/backend/GetMembers/${granularidad}`
+      `http://192.168.1.76/API.Northwind.ISSC811/GetMembers/${granularidad}`
     );
   }
 }
